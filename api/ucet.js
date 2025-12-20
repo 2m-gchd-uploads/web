7dbd824b9a24ee832b9518e0072e842eb956070a
2m-gchd-uploads <ondra.kotan@gmail.com>
2025-12-20 14:06:08 +0100

    Add files via upload

--- api/ucet.js
+++ api/ucet.js
@@ -1,12 +1,10 @@
-export default {
-    ucet(path, request) {
-        switch (path[0]) {
-            case "prihlasit-se":
-                let json = JSON.parse(request.body);
-                json.status = 200;
-                return json;
-            default:
-                return null;
-        }
+export function ucet(path, request) {
+    switch (path[0]) {
+        case "prihlasit-se":
+            let json = JSON.parse(request.body);
+            json.status = 200;
+            return json;
+        default:
+            return null;
     }
 }
