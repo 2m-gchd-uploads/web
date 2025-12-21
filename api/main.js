8265781607b5697d53ce36e3273e6e28821c612f
2m-gchd-uploads <ondra.kotan@gmail.com>
2025-12-21 08:52:58 +0100

    Add files via upload

--- api/main.js
+++ api/main.js
@@ -1,10 +1,22 @@
+function makeResponse(jsonResponse) {
+    return new Response(JSON.stringify(jsonResponse), {
+            headers: { "Content-Type": "application/json" },
+            status: jsonResponse.status
+        });
+}
+
+function notFound() { return { error: "Nenalezeno", status: 404 }; }
+function badRequest(text) { return { error: text, status: 400 }; }
+
 function ucet(path, json) {
     switch (path[0]) {
         case "prihlasit-se":
+            if (json.email == undefined || json.password == undefined)
+                                    { return badRequest("Chybějící pole v požadavku"); }
             json.status = 200;
             return json;
         default:
-            return null;
+            return notFound();
     }
 }
 
@@ -13,25 +25,18 @@ export default {
     const url = new URL(request.url).pathname.split("/").slice(1);
 
     if (url[0] == "api") {
-        let response, json;
-        try {
-            json = await (new Response(request.body).json());
-        } catch (error) {
-            return new Response(JSON.stringify({error: error.message}), {
-                headers: { "Content-Type": "application/json" },
-                status: 400   // 400 Bad Request
-            });
-        }
+        let json;
+
+        try { json = await (new Response(request.body).json());
+        } catch (error) { return makeResponse(badRequest(error.message)); }
+        if (json === null) { return makeResponse(badRequest("body nesmí být null")); }
+
         switch (url[1]) {
             case "ucet":
-                response = ucet(url.slice(2), json);
-                break;
+                return makeResponse(ucet(url.slice(2), json));
+            default:
+                return makeResponse(notFound());
         }
-        if (response == null) { response = { error: "Nenalezeno", status: 404 }; }
-        return new Response(JSON.stringify(response), {
-            headers: { "Content-Type": "application/json" },
-            status: response.status
-        });
     }
 
     return env.ASSETS.fetch(request); // Show 404 error
